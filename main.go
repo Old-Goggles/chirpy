299b6f16197976d96e270bd63f95f98aebf979a9
Nathan Conlon <nconlon@pm.me>
2026-02-26 15:15:51 -0600

    Add database layer with SQLC
    - Configure SQLC for query generation
    - Add CreateUser query
    - Set up PostgreSQL connection with lib/pq
    - Load DB_URL from .env file

--- main.go
+++ main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/Old-Goggles/chirpy/internal/database"
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	database       *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
+	dbQueries := database.New(db)
+
 	const root = "."
 	const port = "8080"
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		database:       dbQueries,
 	}
 
 	mux := http.NewServeMux()
